049849f79c5ef799d3417d646d92ac4d5daa1347
Nikolay Syryaev <zoronbet@gmail.com>
2025-02-10 01:40:27 +0400

    ДЗ по модулю 4

---
+++ DeliveryApp.Core/Domain/Model/CourierAggregate/Courier.cs
@@ -0,0 +1,150 @@
+﻿using CSharpFunctionalExtensions;
+using DeliveryApp.Core.Domain.Model.SharedKernel;
+using Primitives;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DeliveryApp.Core.Domain.Model.CourierAggregate
+{
+    /// <summary>
+    /// Курьер
+    /// </summary>
+    public class Courier : Aggregate<Guid>
+    {
+        /// <summary>
+        /// Имя курьера
+        /// </summary>
+        public string Name { get; }
+        
+        /// <summary>
+        /// Транспорт курьера
+        /// </summary>
+        public Transport Transport { get; }
+
+        /// <summary>
+        /// Текущее местоположение курьера
+        /// </summary>
+        public Location Location { get; private set; }
+
+        /// <summary>
+        /// Статус курьера
+        /// </summary>
+        public CourierStatus Status { get; private set; }
+
+        [ExcludeFromCodeCoverage]
+        private Courier() {}
+
+        /// <summary>
+        /// Конструктор для создания курьера
+        /// </summary>
+        /// <param name="name">Имя курьера</param>
+        /// <param name="transport">Транспорт курьера</param>
+        /// <param name="location"></param>
+        private Courier(string name, Transport transport, Location location) : this()
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Transport = transport;
+            Location = location;
+            Status = CourierStatus.Free;
+        }
+
+        /// <summary>
+        /// Фабричный метод для создания курьера
+        /// </summary>
+        /// <param name="name">Имя курьера</param>
+        /// <param name="transport">Транспорт курьера</param>
+        /// <param name="location">Адрес курьера</param>
+        /// <returns></returns>
+        public static Result<Courier, Error> Create(string name, Transport transport, Location location)
+        {
+            if (name == null) return GeneralErrors.ValueIsRequired(nameof(name).ToLowerInvariant());
+            if (transport == null) return GeneralErrors.ValueIsRequired(nameof(transport).ToLowerInvariant());
+            if (location == null) return GeneralErrors.ValueIsRequired(nameof(location).ToLowerInvariant());
+
+            return new Courier(name, transport, location);
+        }
+
+        /// <summary>
+        /// Сделать курьера свободным
+        /// </summary>
+        /// <returns></returns>
+        public UnitResult<Error> SetFree()
+        {
+            this.Status = CourierStatus.Free;
+            return UnitResult.Success<Error>();
+        }
+
+        /// <summary>
+        /// Сделать курьера занятым
+        /// </summary>
+        /// <returns></returns>
+        public UnitResult<Error> SetBusy()
+        {
+            if (Status == CourierStatus.Busy) return Errors.TryAssignOrderWhenCourierBusy();
+
+            this.Status = CourierStatus.Busy;
+            return UnitResult.Success<Error>();
+        }
+
+        /// <summary>
+        /// Получить кол-во шагов до адреса заказа
+        /// </summary>
+        /// <param name="location">Адрес заказа</param>
+        /// <returns></returns>
+        public Result<double, Error> GetNumberOfSteps(Location location)
+        {
+            if (location == null) return GeneralErrors.ValueIsRequired(nameof(location));
+
+            var distance = this.Location.CalculateDistance(location);
+            return distance / this.Transport.Speed;
+        }
+
+        /// <summary>
+        /// Сделать один шаг в направлении заказа
+        /// </summary>
+        /// <param name="finalLocation">Адрес заказа</param>
+        /// <returns></returns>
+        public Result<Location, Error> MoveStep(Location finalLocation)
+        {
+            if (finalLocation == null) return GeneralErrors.ValueIsRequired(nameof(finalLocation).ToLowerInvariant());
+
+            var oneStep = this.Transport.Speed;
+
+            while (oneStep > 0 && this.Location.X != finalLocation.X)
+            {
+                if (this.Location.X < finalLocation.X)
+                    this.Location = Location.Create(this.Location.X + 1, this.Location.Y).Value;
+                
+                else if (this.Location.X > finalLocation.X)
+                    this.Location = Location.Create(this.Location.X - 1, this.Location.Y).Value;
+               
+                oneStep--;
+            }
+
+            while (oneStep > 0 && this.Location.Y != finalLocation.Y)
+            {
+                if (this.Location.Y < finalLocation.Y)
+                    this.Location = Location.Create(this.Location.X, this.Location.Y + 1).Value;
+                else if (this.Location.Y > finalLocation.Y)
+                    this.Location = Location.Create(this.Location.X, this.Location.Y - 1).Value;
+
+                oneStep--;
+            }
+
+            return this.Location;
+        }
+
+        /// <summary>
+        /// Возможные ошибки сущности курьера
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        public static class Errors
+        {
+            public static Error TryAssignOrderWhenCourierBusy()
+            {
+                return new Error($"{nameof(Courier).ToLowerInvariant} try assign order when courier busy.",
+                    "Нельзя назначить заказ, если курьер уже занят");
+            }
+        }
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/Model/OrderAggregate/OrderShould.cs
@@ -0,0 +1,78 @@
+﻿using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using DeliveryApp.Core.Domain.Model.OrderAggregate;
+using DeliveryApp.Core.Domain.Model.SharedKernel;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Model.OrderAggregate
+{
+    public class OrderShould
+    {
+        public static IEnumerable<object[]> GetIncorrectOrderParams()
+        {
+            yield return [Guid.Empty, Location.MinPoint];
+            yield return [Guid.NewGuid(), null];
+        }
+        [Fact]
+        public void ShouldCreateOrderWhenCorrectParams()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+
+            //Act
+            var result = Order.Create(id, Location.MinPoint);
+
+            //Arrange
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Id.Should().NotBeEmpty();
+            result.Value.Location.Should().Be(Location.MinPoint);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetIncorrectOrderParams))]
+        public void ShouldReturnErrorWhenIncorrectParams(Guid id, Location location)
+        {
+            // Act
+            var result = Order.Create(id, location);
+
+            //Assert
+            result.Error.Should().NotBeNull();
+            result.IsSuccess.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldAssignToCourierWhenParamsIsCorrect()
+        {
+            //Arrange
+            var order = Order.Create(Guid.NewGuid(), Location.MinPoint).Value;
+            var courier = Courier.Create("Test", Transport.Pedestrian, Location.MinPoint).Value;
+
+            //Act
+            var result = order.AssignCourier(courier);
+
+            //Assert
+            result.IsSuccess.Should().BeTrue();
+            order.CourierId.Should().Be(courier.Id);
+            order.Status.Should().Be(OrderStatus.Assigned);
+        }
+
+        [Fact]
+        public void ShouldCompleteOrderWhenCorrectParams()
+        {
+            //Arrange
+            var order = Order.Create(Guid.NewGuid(), Location.MinPoint).Value;
+            var courier = Courier.Create("Test", Transport.Pedestrian, Location.MinPoint).Value;
+            order.AssignCourier(courier);
+
+            //Act
+            var result = order.Complete();
+
+            //Assert
+            result.IsSuccess.Should().BeTrue();
+            order.CourierId.Should().Be(courier.Id);
+            order.Status.Should().Be(OrderStatus.Completed);
+        }
+    }
+}
